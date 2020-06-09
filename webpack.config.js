47c046b330d63089d385ff505b28b031a617d3ad
Stoyan Merdzhanov <stoyan.merdzhanov@gmail.com>
2020-06-09 23:48:47 +0300

    feature(webpack): add  plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin;
 
 // FIXME: Webpack will be used only for production build; development would happen inside the storybook
 // TODO: might need a plugin to include generated CSS https://webpack.js.org/plugins/mini-css-extract-plugin
 // TODO: production guide https://webpack.js.org/guides/production/
 // TODO: we don't need styled components bundled with our lib but either just the generated components with styles OR as peer dependency
-// TODO: bundle size
 
-const BUNDLE_FILENAME = 'js-template.bundle.js';
+const FILENAME = 'js-template';
 const LIB_NAME = 'jsTemplate';
 
 module.exports = {
@@ -16,7 +17,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: BUNDLE_FILENAME,
+    filename: `${FILENAME}.bundle.js`,
     library: LIB_NAME,
     libraryTarget: 'umd'
   },
@@ -31,7 +32,14 @@ module.exports = {
       }
     ]
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+      reportFilename: `${FILENAME}.webpack-bundle-analyzer.html`,
+      openAnalyzer: false
+    })
+  ],
   externals: [nodeExternals()],
   devtool: 'source-map'
 };
